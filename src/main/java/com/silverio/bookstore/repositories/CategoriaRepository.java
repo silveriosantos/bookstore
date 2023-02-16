0d47da1e13c7cb14c1f2639bcad3fca0763d563e
silveriosantos <silveriojannard@gmail.com>
2023-02-16 11:33:04 -0300

    instancia de forma automatica

--- src/main/java/com/silverio/bookstore/repositories/CategoriaRepository.java
+++ src/main/java/com/silverio/bookstore/repositories/CategoriaRepository.java
@@ -3,9 +3,9 @@ package com.silverio.bookstore.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.silverio.bookstore.domain.Livro;
+import com.silverio.bookstore.domain.Categoria;
 
 @Repository
-public interface LivroResository extends JpaRepository <Livro, Integer>{
+public interface CategoriaRepository extends JpaRepository <Categoria, Integer>{
 
 }
--- src/main/java/com/silverio/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/silverio/bookstore/repositories/LivroRepository.java
@@ -3,9 +3,9 @@ package com.silverio.bookstore.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.silverio.bookstore.domain.Categoria;
+import com.silverio.bookstore.domain.Livro;
 
 @Repository
-public interface CategoriaResository extends JpaRepository <Categoria, Integer>{
+public interface LivroRepository extends JpaRepository <Livro, Integer>{
 
 }
